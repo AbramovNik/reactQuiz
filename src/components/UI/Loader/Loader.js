cb40d92fefbe33872c82fc0417ee1b08a97bf516
AbramovNik <abr.nik96@gmail.com>
2020-06-17 18:58:42 +0300

    react-quiz-application

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="Loader-center">
+      <div className="Loader">
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
